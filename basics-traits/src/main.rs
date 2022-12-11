f1763c59676ab2d7acba4af5b15aeadbee218e2e
fentosv <fentos.dev@gmail.com>
2022-12-11 18:47:32 +0100

    Add basics-traits

---
+++ basics-traits/src/main.rs
@@ -0,0 +1,67 @@
+// each type implementing this trait must provide its own custom behavior for the body of the method.
+trait Area {
+    fn area(&self) -> f64;
+}
+
+struct Circle {
+    radius: f64,
+}
+
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+// impl Trait for Type: Trait is the name of the trait being implemented and Type is the name of the implementor struct or the enum.
+impl Area for Circle {
+    fn area(&self) -> f64 {
+        use std::f64::consts::PI;
+        PI * self.radius.powf(2.0)
+    }
+}
+
+impl Area for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+}
+
+fn trait_example() {
+    let circle = Circle { radius: 5.0 };
+    let rectangle = Rectangle {
+        width: 10.0,
+        height: 20.0,
+    };
+
+    println!("Circle area: {}", circle.area());
+    println!("Rectangle area: {}", rectangle.area());
+}
+
+// How to compare
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 4, y: -3 };
+
+    println!("{}", p1);
+    println!("{:?}", p1);
+
+    if p1 == p2 {
+        println!("equal!");
+    } else {
+        println!("not equal!");
+    }
+}
