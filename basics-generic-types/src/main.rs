75211985d7a206d5e32565a9c002961ee143bd69
fentosv <fentos.dev@gmail.com>
2022-12-11 13:38:50 +0100

    Add basics-generic-types

---
+++ basics-generic-types/src/main.rs
@@ -0,0 +1,24 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+struct PointMultiple<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let boolean = Point { x: true, y: false };
+    let integer = Point { x: 1, y: 9 };
+    let float = Point { x: 1.7, y: 4.3 };
+    let string_slice = Point {
+        x: "high",
+        y: "low",
+    };
+
+    let integer_and_boolean = PointMultiple { x: 5, y: false };
+    let float_and_string = PointMultiple { x: 1.0, y: "hey" };
+    let integer_and_float = PointMultiple { x: 5, y: 4.0 };
+    let both_integer = PointMultiple { x: 10, y: 30 };
+    let both_boolean = PointMultiple { x: true, y: true };
+}
