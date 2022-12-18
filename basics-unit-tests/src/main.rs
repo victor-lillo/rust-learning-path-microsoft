b29d9a385dc6622314a7dcfd4421a8c857a2eb8c
fentosv <fentos.dev@gmail.com>
2022-12-18 20:15:52 +0100

    Add basics-unit-tests

---
+++ basics-unit-tests/src/main.rs
@@ -0,0 +1,34 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// cfg controls conditional compilation
+// In this case, 'cargo test' set the flag 'test' to 'true'
+#[cfg(test)]
+mod add_function_tests {
+    use super::*; // To access the add function in the outer module
+
+    #[test]
+    fn add_works() {
+        assert_eq!(add(1, 2), 3);
+        assert_eq!(add(10, 12), 22);
+        assert_eq!(add(5, -2), 3);
+    }
+
+    #[test]
+    fn add_fails() {
+        assert_eq!(add(2, 2), 7);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_expected_fails() {
+        assert_eq!(add(2, 2), 7);
+    }
+
+    #[test]
+    #[ignore = "not yet reviewed by the Q.A. team"]
+    fn add_negatives() {
+        assert_eq!(add(-2, -2), -4)
+    }
+}
