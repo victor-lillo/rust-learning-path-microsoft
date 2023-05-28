d942fac9cd9bc3ddb177239ae0fef70a33a90926
fentosv <fentos.dev@gmail.com>
2023-05-28 18:11:38 +0200

    Update 11-basics-impl code

--- 11-basics-impl/src/main.rs
+++ 11-basics-impl/src/main.rs
@@ -6,16 +6,26 @@ struct Salad {
 }
 
 impl Salad {
+    // When using 'self', we can use the method from any struct instance
     fn get_price_per_ingredient(&self) -> f32 {
         let ingredients_number: f32 = self.ingredients.len() as f32;
         let price: f32 = self.price as f32;
 
         price / ingredients_number
     }
+
+    // When not using 'self', we can use the method from the Struct
+    fn eat() {
+        println!("Salad eaten!");
+    }
+
+    fn set_price(&mut self, new_price: u8) {
+        self.price = new_price;
+    }
 }
 
 fn main() {
-    let vegan_salad = Salad {
+    let mut vegan_salad = Salad {
         name: "Vegan".to_string(),
         ingredients: vec![
             "Lettuce".to_string(),
@@ -26,8 +36,16 @@ fn main() {
         price: 15,
     };
 
-    println!("{:?}", vegan_salad);
+    println!("Salad data: {:?}", vegan_salad);
 
+    // Using the impl method from the instance
     let price_per_ingredient = vegan_salad.get_price_per_ingredient();
-    println!("{}", price_per_ingredient);
+    println!("The average price per ingredient is: {price_per_ingredient}");
+
+    // Modify the instance data
+    vegan_salad.set_price(18);
+    println!("The new price is: {}", vegan_salad.price);
+
+    // Using the impl method from the struct
+    Salad::eat();
 }
